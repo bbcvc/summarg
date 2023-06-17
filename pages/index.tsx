e95a80fc48c4fadf5a83cae33eb3cb5258071346
chenglong <chenglong9604@outlook.com>
2023-06-17 16:05:44 +0800

    chore: init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import useSWR from 'swr'
 
 const inter = Inter({ subsets: ['latin'] })
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export default function Home() {
+
+  const {data} = useSWR('api/hello', fetcher)
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
